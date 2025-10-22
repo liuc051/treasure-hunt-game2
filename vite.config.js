009070415548e496d1bb198d980e497c66c1121c
liuc051 <2862552063@qq.com>
2025-10-23 06:21:27 +0800

    修复资源引入路径

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/treasure-hunt-game2/'
+  base: '/treasure-hunt-game2/',
+    build: {
+    entry: 'src/main.js', // 明确入口文件
+  }
 })
